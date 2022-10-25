9dc8073c9bb937efcadf48266895a124ced8158b
Miguel Salvador Cruz Rodríguez <miguelsalvador@gmail.com>
2022-10-25 16:52:02 -0500

    security error

--- src/main/java/com/tcs/demo/config/WebSecurityConfig.java
+++ src/main/java/com/tcs/demo/config/WebSecurityConfig.java
@@ -1,17 +1,48 @@
 package com.tcs.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests().
-                antMatchers("/","index","/css/","/js/*")
+                antMatchers("/","/index","/css/","/js/*")
                 .permitAll().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails sergioUser = User.builder().username("Sergio").password(passwordEncoder.encode("123")).build();
+        return new InMemoryUserDetailsManager(sergioUser);
     }
 }
